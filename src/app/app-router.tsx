2c96df0a6dac099026ddfe44638f8426d367e1c8
vvebninja <aleksandru.morar@gmail.com>
2025-12-15 22:23:26 +0200

    fix(deploy): Set basename for React Router and Vite config for GitHub Pages

--- src/app/app-router.tsx
+++ src/app/app-router.tsx
@@ -2,21 +2,26 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import { routePaths } from '@/constants';
 import { App } from './app';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: routePaths.HOME,
+      element: <App />,
+      children: [
+        {
+          index: true,
+          lazy: async () => await import('@/pages/home/home-page'),
+        },
+        {
+          path: routePaths.CART,
+          lazy: async () => await import('@/pages/cart/cart-page'),
+        },
+      ],
+    },
+  ],
   {
-    path: routePaths.HOME,
-    element: <App />,
-    children: [
-      {
-        index: true,
-        lazy: async () => await import('@/pages/home/home-page'),
-      },
-      {
-        path: routePaths.CART,
-        lazy: async () => await import('@/pages/cart/cart-page'),
-      },
-    ],
+    basename: '/react-pizza/',
   },
-]);
+);
 
 export const AppRouter = () => <RouterProvider router={router} />;
