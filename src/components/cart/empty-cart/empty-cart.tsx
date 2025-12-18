fb023226ae5d9bb6db3b931339deb50818c1e9b3
vvebninja <aleksandru.morar@gmail.com>
2025-12-18 17:05:32 +0200

    feat: Add empty cart ui component

---
+++ src/components/cart/empty-cart/empty-cart.tsx
@@ -0,0 +1,32 @@
+import ShoppingCart from '@/assets/img/shopping-cart-img.svg?react';
+import css from './empty-cart.module.scss';
+import { routePaths } from '@/constants/constants.ts';
+import { LinkButton } from '@/components/ui/link-button/link-button';
+import type { FC } from 'react';
+import clsx from 'clsx';
+
+type EmptyCartProps = {
+  className?: string;
+};
+
+export const EmptyCart: FC<EmptyCartProps> = ({ className }) => {
+  return (
+    <section className={clsx(css.empty_cart, className)}>
+      <h2 className={css.empty_cart_title}>Cart is empty 😕</h2>
+      <p className={css.empty_cart_subtitle}>
+        It looks like you don't have any pizza orders yet.{' '}
+        <br className={css.empty_cart_line_breake} />
+        To get started, just head over to the homepage!
+      </p>
+      <ShoppingCart className={css.empty_cart_img} />
+      <LinkButton
+        size="lg"
+        variant="solid"
+        color="dark"
+        to={routePaths.HOME}
+      >
+        Go back
+      </LinkButton>
+    </section>
+  );
+};
